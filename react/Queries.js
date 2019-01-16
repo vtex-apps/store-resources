dcb1222ae67d7ff62eff5ff1e5bdfc3ee0039956
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-16 09:12:48 -0200

    Add more queries and rename the .gql files

--- react/Queries.js
+++ react/Queries.js
@@ -1,12 +1,16 @@
-import session from './queries/sessionQuery.gql'
-import orderForm from './queries/orderFormQuery.gql'
-import product from './queries/productQuery.gql'
-import recommendationsAndBenefits from './queries/recommendationsAndBenefitsQuery.gql'
-import search from './queries/searchQuery.gql'
+import orderForm from './queries/orderForm.gql'
+import product from './queries/product.gql'
+import products from './queries/products.gql'
+import productPreviewFragment from './queries/productPreview.gql'
+import recommendationsAndBenefits from './queries/recommendationsAndBenefits.gql'
+import search from './queries/search.gql'
+import session from './queries/session.gql'
 
 export default {
   orderForm,
   product,
+  products,
+  productPreviewFragment,
   recommendationsAndBenefits,
   search,
   session,
