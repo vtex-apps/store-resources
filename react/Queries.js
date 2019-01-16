9ca8834773472f0edb22752dd5ea8293d0232488
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-16 09:14:09 -0200

    Remove products query

--- react/Queries.js
+++ react/Queries.js
@@ -1,6 +1,5 @@
 import orderForm from './queries/orderForm.gql'
 import product from './queries/product.gql'
-import products from './queries/products.gql'
 import productPreviewFragment from './queries/productPreview.gql'
 import recommendationsAndBenefits from './queries/recommendationsAndBenefits.gql'
 import search from './queries/search.gql'
@@ -9,7 +8,6 @@ import session from './queries/session.gql'
 export default {
   orderForm,
   product,
-  products,
   productPreviewFragment,
   recommendationsAndBenefits,
   search,
