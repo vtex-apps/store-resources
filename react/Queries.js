ea4855329537f56f3275fe72d0f7a316c75cb2db
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-25 13:36:58 -0300

    Move new query to separate gql

--- react/Queries.js
+++ react/Queries.js
@@ -3,11 +3,13 @@ import product from './queries/product.gql'
 import productPreviewFragment from './queries/productPreview.gql'
 import recommendationsAndBenefits from './queries/recommendationsAndBenefits.gql'
 import search from './queries/search.gql'
+import productSearch from './queries/productSearch.gql'
 import session from './queries/session.gql'
 
 export default {
   orderForm,
   product,
+  productSearch,
   productPreviewFragment,
   recommendationsAndBenefits,
   search,
