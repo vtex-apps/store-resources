d120c05a87c6bd7f7ceb5335b08dcd9bb54c072b
Hiago Lucas Cardeal <hcardealdemelo@gmail.com>
2020-03-20 14:09:41 -0300

    remove searchMetadaV2 from queries.js

--- react/Queries.js
+++ react/Queries.js
@@ -8,12 +8,10 @@ import recommendationsAndBenefits from './queries/recommendationsAndBenefits.gql
 import search from './queries/search.gql'
 import productSearch from './queries/productSearch.gql'
 import productSearchV2 from './queries/productSearchV2.gql'
-import productSearchV3 from './queries/productSearchV3.gql'
 import session from './queries/session.gql'
 import productBenefits from './queries/productBenefits.gql'
 import address from './queries/address.gql'
 import searchMetadata from './queries/searchMetadata.gql'
-import searchMetadataV2 from './queries/searchMetadataV2.gql'
 import productCategoryTree from './queries/UNSTABLE__productCategoryTree.gql'
 import facets from './queries/facets.gql'
 
@@ -30,6 +28,5 @@ export default {
   productBenefits,
   UNSTABLE__productCategoryTree: productCategoryTree,
   searchMetadata,
-  searchMetadataV2,
   facets
 };
